5ff4dbbc9eafdf8f11ac2201a5a2e1af8a76f568
Anita <karrpd@juno.com>
2017-03-13 18:50:18 -0400

    replaced button with slider, redraw pyramid when symbol changes, added heigh counter

--- pyramid-slide.js
+++ pyramid-slide.js
@@ -5,69 +5,73 @@
  * Determines the current value that the user has typed in the 'How high?' text-box,
  * and then draws a pyramid with that height.
  */
+ //figure out what symbol the user selected (i.e. @, #, X, $)
+
+function getSymbol(){
+ var select = document.getElementById("brick").value;
+// console.log(select)
+  return select;
+ }
+
 function determineHeightAndThenDrawPyramid() {
 
     // just so we know we're here
     console.log("someone invoked the determineHeightAndThenDrawPyramid function!");
+    //console.log(getSymbol());
+    console.log(height);
 
-    // TODO 3
     // figure out the height the user typed (replace the "5" below)
-      //heightStr = "5";
    heightStr = document.getElementById("height").value;
-   //heightStr = document.querySelector("#heightStr");
 
     // here we convert the string to an int
-    height = parseInt(heightStr);
+   height = parseInt(heightStr);
 
-    // TODO 2
-    // draw the pyramid with the given height
-    drawPyramid(height);
-
+    // draw the pyramid with the given height and symbol
+    drawPyramid(height, getSymbol());
 
 }
 
-// TODO 1
-// hook up the button's click event to our determineHeightAndThenDrawPyramid function
-  var button = document.querySelector("#button");
-  button.addEventListener("click", determineHeightAndThenDrawPyramid);
-  // document.getElementById("myBtn").addEventListener("click", myFunction);
+//hook up the slider's oninput event to our determineHeightAndThenDrawPyramid function
+       document.getElementById("height").oninput = determineHeightAndThenDrawPyramid;
 
+//when user selects a different symbol, re-render the pyramid
+        document.getElementById("brick").onchange = determineHeightAndThenDrawPyramid;
 
 /**
  * drawPyramid
  *
  * Renders, in the HTML document, a Mario pyramid of the specified height
  */
- function drawPyramid(height) {
+ function drawPyramid(height, symbol) {
 
-     // TODO 4
      // before drawing, clear the old content
-      document.getElementById("pyramid").innerHTML = "";
+        document.getElementById("pyramid").innerHTML = "";
+        document.getElementById("heightnum").innerHTML = height ;
+
 
      // for each row....
      for (var row = 0; row < height; row++) {
 
-         // figure out number of bricks and spaces
-         var numBricks = row + 2;
-         var numSpaces = height - row - 1;
+        // figure out number of bricks and spaces
+        var numBricks = row + 2;
+        var numSpaces = height - row - 1;
 
-         // build up a string for this row
-         var rowStr = "";
-         for (var i = 0; i < numSpaces; i++) {
-             rowStr += ".";
-         }
-         for (var i = 0; i < numBricks; i++) {
-             rowStr += "#";
-         }
-
+        // build up a string for this row
+        var rowStr = "";
+        for (var i = 0; i < numSpaces; i++) {
+            var spaceChar = "&nbsp"; // this is the HTML encoding for a space " "
+            rowStr += spaceChar;
+        }
+        for (var i = 0; i < numBricks; i++) {
+            // rowStr += "#";
+            rowStr += symbol;
+        }
         // create a text element with the string of characters
-        textElem = document.createTextNode(rowStr);
 
-        // create a <p> element with the text inside
         rowElem = document.createElement("p");
-        rowElem.appendChild(textElem);
-
-        // insert the paragraph as a child of the container <div>
+        rowElem.innerHTML = rowStr;
         document.getElementById("pyramid").appendChild(rowElem);
+
+        
     }
 }
